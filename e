688b00d7ffd0b23434a770bb30d1c4bc8a943b5f
pythonideas <rustbotchessapp@gmail.com>
2021-02-05 08:30:43 +0000

    Test publish

--- e
+++ e
@@ -1,7 +1,9 @@
 eval $(gp env -e)
 
 cp lichessbot/pyvenv.cfg /home/gitpod/.cache/pypoetry/virtualenvs/lichessbot-IEoI1RvR-py3.8/pyvenv.cfg
+cp test/pyvenv.cfg /home/gitpod/.cache/pypoetry/virtualenvs/test-rMMDreiZ-py3.8/pyvenv.cfg
 
 ls /home/gitpod/.cache/pypoetry/virtualenvs/lichessbot-IEoI1RvR-py3.8/ -al
+ls /home/gitpod/.cache/pypoetry/virtualenvs/test-rMMDreiZ-py3.8/ -al
 
 poetry config repositories.testpypi https://test.pypi.org/legacy/
---
+++ test/main.py
@@ -0,0 +1,28 @@
+import asyncio
+import lichessbotpoetry.api as api
+import os
+
+task = None
+
+async def stream():    
+  global task
+  task = asyncio.create_task(api.stream("api/stream/event", os.getenv("RUSTBOT_TOKEN")))
+  print("task", task)
+  await task
+  print("task done")
+
+async def main():  
+    await asyncio.gather(stream())
+
+while True:
+  try:
+    asyncio.run(main())
+  except KeyboardInterrupt:
+    if not task is None:
+      print("cancelling task")
+      task.cancel()
+    else:
+      print("no task to cancel")
+  stop = input("stop?")
+  if ( stop == "y" ) or ( stop == "Y" ):
+    break
