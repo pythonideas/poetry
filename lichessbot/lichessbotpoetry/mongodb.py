ec0d895eee975347c45364b35d8d565f7ccf0a49
pythonideas <rustbotchessapp@gmail.com>
2021-02-06 13:38:02 +0000

    Token collection

--- lichessbot/lichessbotpoetry/mongodb.py
+++ lichessbot/lichessbotpoetry/mongodb.py
@@ -8,7 +8,16 @@ from pprint import pprint
 # connect to MongoDB, change the << MONGODB URL >>
 # to reflect your own connection string
 client = MongoClient(env.get("MONGODB_URI"))
-db = client.admin
+#db = client.admin
 # Issue the serverStatus command and print the results
-serverStatusResult = db.command("serverStatus")
-pprint(serverStatusResult)
+#serverStatusResult = db.command("serverStatus")
+#pprint(serverStatusResult)
+
+class Token():
+    def __init__(self):
+        self.token = None
+        self.username = None
+
+bulk_db = client.bulk
+
+tokens_coll = bulk_db.tokens
