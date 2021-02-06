3fa031cd75bbfac348d46ad0310692571374d40e
pythonideas <rustbotchessapp@gmail.com>
2021-02-06 13:06:00 +0000

    Mongodb

---
+++ lichessbot/lichessbotpoetry/mongodb.py
@@ -0,0 +1,14 @@
+import os
+
+from pymongo import MongoClient
+
+# pprint library is used to make the output look more pretty
+from pprint import pprint
+
+# connect to MongoDB, change the << MONGODB URL >>
+# to reflect your own connection string
+client = MongoClient(os.environ.get("MONGODB_URI"))
+db = client.admin
+# Issue the serverStatus command and print the results
+serverStatusResult = db.command("serverStatus")
+pprint(serverStatusResult)
